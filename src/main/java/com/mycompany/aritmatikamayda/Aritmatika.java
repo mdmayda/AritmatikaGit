7d1a02a0c7cca37c39a2032a67290ff8b91cb072
mdmayda <mdmayda123@gmail.com>
2022-12-16 08:32:33 +0700

    Ini adalah inisiasi projek aritmatika

---
+++ src/main/java/com/mycompany/aritmatikamayda/Aritmatika.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aritmatikamayda;
+
+/**
+ *
+ * @author User
+ */
+public class Aritmatika {
+    private double bilsatu;
+    private double bildua;
+
+    public Aritmatika(double bilsatu, double bildua) {
+        this.bilsatu = bilsatu;
+        this.bildua = bildua;
+    }
+
+    public double getBilsatu() {
+        return bilsatu;
+    }
+
+    public void setBilsatu(double bilsatu) {
+        this.bilsatu = bilsatu;
+    }
+
+    public double getBildua() {
+        return bildua;
+    }
+
+    public void setBildua(double bildua) {
+        this.bildua = bildua;
+    }
+    
+    //Penjumlahan
+    public double penjumlahan() {
+        return 0;
+    }
+    
+    //Pengurangan
+    public double pengurangan(){
+        return 0;
+    }
+    
+    //Perkalian
+    public double perkalian() {
+        return 0;
+    }
+    
+    //Pembagian
+    public double pembagian() {
+        return 0;
+    }
+}
